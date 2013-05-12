b919b2415a9ff70d37e663ce643850c31dffdf1f
Gareth J Barnard <gjbarnard@gmail.com>
2013-05-12 12:53:52 +0100

    V2.5.0.2

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013050900;
+$plugin->version = 2013051200;
 $plugin->maturity = MATURITY_BETA;
 $plugin->requires = 2013050200.00; // 2.5beta+ (Build: 20130502).
 $plugin->component = 'format_columns';
